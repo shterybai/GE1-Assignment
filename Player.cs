a326d0347f180015eb1cbffdc10a291103ee7f6c
shterybai <andrewkennan2000@gmail.com>
2022-12-20 18:23:41 +0000

    Normalized movement direction

--- Player.cs
+++ Player.cs
@@ -9,9 +9,16 @@ public class Player : KinematicBody
     [Export]
     public int FallAcceleration = 5;
     [Export]
-    public int JumpForce = 5;
+    public int JumpForce = 10;
+    
+    // Camera Settings
+    public float MinLookAngle = -90;
+    public float MaxLookAngle = 90;
+    public float MouseSensitivity = 5;
 
+    // Vector Settings
     private Vector3 _velocity = Vector3.Zero;
+    private Vector2 MouseDelta = Vector2.Zero;
 
     public override void _PhysicsProcess(float delta)
     {
@@ -19,23 +26,21 @@ public class Player : KinematicBody
         var direction = Vector3.Zero;
 
         // We check for each move input and update the direction accordingly
-        if (Input.IsActionPressed("ui_right"))
-        {
+        if (Input.IsActionPressed("ui_right")) {
             direction.x += Speed;
         }
-        if (Input.IsActionPressed("ui_left"))
-        {
+        if (Input.IsActionPressed("ui_left")) {
             direction.x -= Speed;
         }
-        if (Input.IsActionPressed("ui_backward"))
-        {
-            // Notice how we are working with the vector's x and z axes.
-            // In 3D, the XZ plane is the ground plane.
+        if (Input.IsActionPressed("ui_backward")) {
             direction.z += Speed;
         }
-        if (Input.IsActionPressed("ui_forward"))
-        {
+        if (Input.IsActionPressed("ui_forward")) {
             direction.z -= Speed;
+        }
+        if (direction != Vector3.Zero) {
+            direction = direction.Normalized();
+            // GetNode<Spatial>("Pivot").LookAt(Translation + direction, Vector3.Up);
         }
 
         MoveAndSlide(direction);
